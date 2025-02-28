ac1dc82c314c53c185d433f74eb6d307a46d2dd6
ben2037 <b.eden2037@gmail.com>
2025-02-28 16:44:52 +0000

    first tables completed feb 28

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+Instructions for setting up your .env files:
+
+
+Create a new file called .env.test
+This will be your test environment
+
+Create a new file called .env.development
+This will be your development environment
+
+
+
+In .env.test, write the following code:
+
+PGDATABASE=nc_news_test
+
+This will complete the connection to the test database
+
+
+In .env.devlopment, write the following code:
+
+PGDATABASE=nc_news
+
+This will complete the connection to the development database
+
+
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,49 @@
 const db = require("../connection")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
-};
+  return db.query(`DROP TABLE IF EXISTS topics`
+).then(() => {
+
+  return db.query(`DROP TABLE IF EXISTS users`)
+}).then(() => {
+
+  return db.query(`DROP TABLE IF EXISTS articles`)
+}).then(() => {
+
+  return db.query(`DROP TABLE IF EXISTS comments`)
+}).then(() => {
+
+  return db.query(`CREATE TABLE topics (
+    slug VARCHAR(100) PRIMARY KEY,
+    description VARCHAR(200),
+    img_url VARCHAR(1000)
+  )`)
+}).then(() => {
+
+  return db.query(`CREATE TABLE users (
+    username VARCHAR(100) PRIMARY KEY,
+    name VARCHAR(100),
+    avatar_url VARCHAR(1000)
+    )`)
+}).then(() => {
+
+  return db.query(`CREATE TABLE articles (
+    article_id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    topic VARCHAR(100) REFERENCES topics(slug),
+    author VARCHAR(100) REFERENCES users(username),
+    body TEXT
+ )`)
+}).then(() => {
+  
+  return db.query(``)
+})
+
+
+}
+
+
+
+
+
 module.exports = seed;
